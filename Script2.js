14565e5e1028807c2d15bd0e560b1f881bf3d840
Shubhangi Thakur <thakurshubhangi999@gmail.com>
2024-07-24 00:19:54 +0530

    NEWME Passowrd Generator using html, css and js

---
+++ Script2.js
@@ -0,0 +1,59 @@
+let inputSlider = document.getElementById("pwRange");
+let sliderValue = document.getElementById("sliderValue");
+let passBox = document.getElementById("pwField");
+let lowercaseCheckbox = document.getElementById("lowercase");
+let uppercaseCheckbox = document.getElementById("uppercase");
+let numbersCheckbox = document.getElementById("numbers");
+let symbolsCheckbox = document.getElementById("symbols");
+let generatorButton = document.getElementById("genBtn");
+let copyPassword = document.getElementById("copyPass");
+
+// showing input slider value
+sliderValue.textContent = inputSlider.value;
+inputSlider.addEventListener("input", () => {
+  sliderValue.textContent = inputSlider.value;
+});
+
+generatorButton.addEventListener("click", () => {
+  passBox.value = generatorPassword();
+});
+
+let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+let allNumbers = "0123456789";
+let allSymbols = "~!@#$%^&*";
+
+// function to generate password
+function generatorPassword() {
+  let genPass = "";
+  let allChars = "";
+
+  allChars += lowercaseCheckbox.checked ? lowerChars : "";
+  allChars += uppercaseCheckbox.checked ? upperChars : "";
+  allChars += numbersCheckbox.checked ? allNumbers : "";
+  allChars += symbolsCheckbox.checked ? allSymbols : "";
+
+  if (allChars == "" || allChars.length == 0) {
+    return genPass;
+  }
+
+  let i = 1;
+  while (i <= inputSlider.value) {
+    genPass += allChars.charAt(Math.floor(Math.random() * allChars.length));
+    i++;
+  }
+  return genPass;
+}
+
+copyPassword.addEventListener("click", () => {
+  if (passBox.value != "" || passBox.value >= 1) {
+    navigator.clipboard.writeText(passBox.value);
+    copyPassword.innerText = "check";
+    copyPassword.title = "Password Copied";
+
+    setTimeout(()=>{
+        copyPassword.innerHTML = "content_copy"
+        copyPassword.title = "";
+    }, 3000)
+  }
+});
